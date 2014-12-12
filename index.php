facf4ffcf2b83cf10eb203f0b7a12472a270cf23
Merlin Mason <info@merlinmason.co.uk>
2014-12-12 11:02:03 +0000

    PARTYTYYYYYY

--- index.php
+++ index.php
@@ -1,42 +1,80 @@
 <?php get_header(); ?>
-
 <div class="party">
+    <div class="bg"></div>
+    <div class="bg bgoriginal"></div>
 
-    <div class="timer-wrapper">
-        <div class="timer"></div>
-    </div>
-
-    <div class="machine">
-        <div class="inner" id="inner">
-            <?php
-                $args = array('numberposts' => 8, 'category' => '2', 'orderby' => 'rand');
-                $lastposts = get_posts( $args );
-                foreach($lastposts as $post) : setup_postdata($post);
-            ?>
-
-                <?php if (has_post_thumbnail( $post->ID ) ): ?>
-                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                    <div class="panel" style="background-image: url('<?php echo $image[0]; ?>')" data-name="<?php the_title(); ?>"></div>
-                <?php endif; ?>
-
-            <?php
-                endforeach;
-                wp_reset_query();
-            ?>
+    <div class="timer-outer-wrapper">
+        <div class="timer-wrapper">
+            <div class="timer"></div>
         </div>
-        <div class="mask"></div>
     </div>
+
+    <div class="bird"></div>
+
+    <div class="outer-machine">
+        <div class="machine">
+            <div class="inner" id="inner">
+                <?php
+                    $args = array('numberposts' => 8, 'category' => '2', 'orderby' => 'rand');
+                    $lastposts = get_posts( $args );
+                    foreach($lastposts as $post) : setup_postdata($post);
+                ?>
+
+                    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                        <div class="panel" style="background-image: url('<?php echo $image[0]; ?>')" data-name="<?php the_title(); ?>"></div>
+                    <?php endif; ?>
+
+                <?php
+                    endforeach;
+                    wp_reset_query();
+                ?>
+            </div>
+            <div class="mask"></div>
+        </div>
+        <div class="light"></div>
+        <div class="light light2"></div>
+        <div class="light light3"></div>
+        <div class="light light4"></div>
+        <div class="light light5"></div>
+    </div>
+
+    <div class="pole"></div>
 
     <div class="winner"></div>
+
+    <div class="choice"></div>
+    <button class="choice-shot" data-action="shot"></button>
+    <button class="choice-dare" data-action="dare"></button>
+
+    <div class="flames"></div>
 
     <?php
         $args = array('numberposts' => 1, 'category' => '3', 'orderby' => 'rand');
         $lastposts = get_posts( $args );
         foreach($lastposts as $post) : setup_postdata($post);
     ?>
+        <div class="the-dare" style="background-image:url(<?php bloginfo('template_url'); ?>/img/dareSkull.png);">
+            <div class="text">
+                <?php the_title(); ?>
+            </div>
+        </div>
+    <?php
+        endforeach;
+        wp_reset_query();
+    ?>
 
-        <div class="dare">Shot or <?php the_title(); ?></div>
-
+    <?php
+        $args = array('numberposts' => 1, 'category' => '4', 'orderby' => 'rand');
+        $lastposts = get_posts( $args );
+        foreach($lastposts as $post) : setup_postdata($post);
+    ?>
+        <div class="the-shot" style="background-image:url(<?php bloginfo('template_url'); ?>/img/shotglass.png);">
+            <div class="text">
+                SHOT!<br>
+                <?php the_title(); ?>
+            </div>
+        </div>
     <?php
         endforeach;
         wp_reset_query();
@@ -46,8 +84,14 @@
 
     <div class="sprite-test"></div>
 
+    <div class="reset"></div>
+
     <audio id="horn" src="<?php bloginfo('template_url'); ?>/audio/horn.mp3"></audio>
     <audio id="machine" src="<?php bloginfo('template_url'); ?>/audio/machine.mp3"></audio>
+    <audio id="pop1" src="<?php bloginfo('template_url'); ?>/audio/pop1.mp3"></audio>
+    <audio id="pop2" src="<?php bloginfo('template_url'); ?>/audio/pop2.mp3"></audio>
+    <audio id="clap" src="<?php bloginfo('template_url'); ?>/audio/clap.mp3"></audio>
+    <div class="bg bgmask"></div>
 </div>
 
 <?php get_footer(); ?>
--- js/project.js
+++ js/project.js
@@ -3,10 +3,17 @@
 
     var party = {
 
-        wait: 3, // in seconds before starting the party
+        wait: 300, // in seconds before starting the party
 
         init: function () {
             party.countDown();
+            party.bindActions();
+        },
+
+        bindActions: function () {
+            $(".reset").on("click", function () { party.partyAgain(); });
+            $(".choice-shot").on("click", function () { party.hideChoice("shot"); });
+            $(".choice-dare").on("click", function () { party.hideChoice("dare"); });
         },
 
         countDown: function () {
@@ -14,21 +21,48 @@
                 countdown: true,
                 clockFace: "MinuteCounter"
             });
+            var bird = $(".bird");
 
             var myInterval = setInterval(function () {
-                console.log(clock.getTime().time);
                 if (clock.getTime().time === 0) {
                     clearInterval(myInterval);
                     setTimeout(function () {
                         party.setStage();
+                        TweenMax.to(bird, 0.5, { transform: "translateX(-800px)", ease: Bounce.easeOut });
+                        birdflapTl.pause();
                     }, 1500);
                 }
             }, 1000);
+
+
+            var birdflapTl = new TimelineMax({
+                delay: 0,
+                repeat: 999,
+            });
+
+            var birdmoveTl = new TimelineMax({
+                delay: 10,
+                repeatDelay: 10,
+                repeat: 999
+            });
+
+            birdmoveTl.to(bird, 10, { transform: "translateX(0px)" })
+            .to(bird, 1, { rotation: 360 }, 5)
+            .to(bird, 10, { transform: "translateX(800px)" }, 20)
+            .to(bird, 1, { rotation: 360 }, 5)
+            .to(bird, 10, { transform: "translateX(-100px)" }, 20)
+            .to(bird, 10, { transform: "translateX(800px)" }, 20);
+
+            birdflapTl.to(bird, 0.48, { backgroundPosition: "-5500px 0", ease: SteppedEase.config(11)})
+            .set(bird, { backgroundPosition: "0px 0px"});
+
         },
 
         setStage: function () {
-            var timer = $(".timer-wrapper");
-            var machine = $(".machine");
+            var timer = $(".timer-outer-wrapper");
+            var machine = $(".outer-machine");
+            var bg = $(".bgoriginal");
+            var pole = $(".pole");
 
             var playHorn = function () { $("#horn").get(0).play(); };
 
@@ -43,19 +77,27 @@
 
             setStage
             .add("start")
-            .to(timer, 0.5, { transform: "translateY(-700px)", ease: Back.easeIn })
+            .to(timer, 0.5, { transform: "translateY(-700px)", ease: Back.easeIn }, "+=0.5")
+            .set(bg, { opacity: 0 }, "+=0.5")
             .addCallback(playHorn)
-            .set(machine, { transform: "translateY(-700px)", opacity: 1 })
-            .to(machine, 0.5, { transform: "translateY(0px)", ease: Bounce.easeOut }, "+=0.5")
+            .set(machine, { transform: "translateY(500px)", opacity: 1 })
+            .add("showMachine")
+            .to(machine, 1, { transform: "translateY(-120px)", ease: Bounce.easeOut }, "showMachine")
+            .to(pole, 1, { transform: "translateY(-220px)", ease: Bounce.easeOut }, "showMachine")
             .addCallback(startMachine);
         },
 
         startMachine: function () {
+            $(".bird").remove();
             // the wrapper we shall spin
             var inner = $("#inner");
             var winnerSign = $(".winner");
-            var dareSign = $(".dare");
-            var light = $(".spotlight");
+            var machine = $(".outer-machine");
+            var pole = $(".pole");
+            var choice = $(".choice");
+            var light = $(".light");
+            var shot = $(".choice-shot");
+            var dare = $(".choice-dare");
             // random number between 1 -8
             var winner = Math.ceil(Math.random() * 8);
             // get winners name from DOM
@@ -64,40 +106,117 @@
             // spin two whole roatations plus amount for winner * deg for each panel
             var spin = ((9 - winner) + 16) * 45;
 
+            var lightTl = new TimelineMax({
+                delay: 0,
+                repeat: 999,
+            });
+
+            lightTl.staggerTo(light, 0.3, { backgroundPosition: "-3434px 0", ease: SteppedEase.config(17)}, 0.3)
+            .set(light, { backgroundPosition: "0px 0px"});
 
             var master = new TimelineMax({
                 delay: 1,
                 repeat: 0,
             });
 
-
+            var stopLight = function () {
+                lightTl.pause();
+            };
 
             var playMachine = function () { $("#machine").get(0).play(); };
+            var playPop1 = function () {
+                $("#pop1").get(0).play();
+            };
+            var playPop2 = function () {
+                $("#pop2").get(0).play();
+            };
 
             // start spinning
-            master
-            .add("start")
-            .to(light, 1, { opacity: "0", ease: Back.easeOutBounce })
-            .to(light, 1, { opacity: "1", ease: Back.easeOutBounce })
+            master.add("start")
+            //.to(light, 1, { opacity: "0", ease: Back.easeOutBounce })
+            //.to(light, 1, { opacity: "1", ease: Back.easeOutBounce })
             .add("spin")
             .to(inner, 6, { rotationX: spin })
-            .add("showSigns", "-=0.5")
-            .addCallback(playMachine, "showSigns")
-            .addCallback(playMachine, "showSigns+=0.25")
-            .to(".sprite-test", 0.8, { backgroundPosition: "-1920px 0", ease: SteppedEase.config(15)})
-            .set(".sprite-test", { backgroundPosition: "0px 0px"})
-            .to(".sprite-test", 0.8, { backgroundPosition: "-1920px 0", ease: SteppedEase.config(15)})
-            .set(".sprite-test", { backgroundPosition: "0px 0px"})
+            .add("showWinner", "-=0.5")
+            .addCallback(playMachine, "showWinner")
+            //.to(".sprite-test", 0.8, { backgroundPosition: "-1920px 0", ease: SteppedEase.config(15)})
+            //.set(".sprite-test", { backgroundPosition: "0px 0px"})
             // add timeline marker half second before end of spin
             // show sign at marker
-            .to(winnerSign, 0.5, { transform: "translateY(-280px)", ease: Back.easeOut }, "showSigns")
+            .to(winnerSign, 0.5, { transform: "translateY(0)", ease: Bounce.easeOut }, "showWinner")
+            .add("hideMachine", "showWinner+=3")
+            .to(machine, 0.5, { transform: "translateY(800px)", ease: Bounce.easeOut }, "hideMachine")
+            .to(pole, 0.5, { transform: "translateY(800px)", ease: Bounce.easeOut }, "hideMachine")
+            .addCallback(playMachine, "hideMachine")
+            .add("showChoice")
             // show second sign relative to marker
-            .to(dareSign, 0.5, { transform: "translateY(-280px)", ease: Back.easeOut }, "showSigns+=0.25");
+            .addCallback(playMachine, "showChoice")
+            .to(choice, 0.5, { transform: "translateY(-40px)", ease: Back.easeOut }, "showChoice")
+            .to(pole, 0.5, { transform: "translateY(-280px)", ease: Back.easeOut }, "showChoice")
+            .to(dare, 0.3, { transform: "scale(1)", ease: Bounce.easeOut }, "showChoice+=1")
+            .to(shot, 0.3, { transform: "scale(1)", ease: Bounce.easeOut }, "showChoice+=1.15")
+            .addCallback(playMachine, "hideMachine")
+            .addCallback(playPop1, "showChoice+=1")
+            .addCallback(playPop2, "showChoice+=1.15")
+            .addCallback(stopLight, "showChoice+=1.15");
         },
 
-        refreshPage: function () {
+        partyAgain: function () {
             location.reload();
-        }
+        },
+
+        hideChoice: function (next) {
+            var pole = $(".pole");
+            var choice = $(".choice");
+            var shot = $(".choice-shot");
+            var dare = $(".choice-dare");
+
+
+            $("#clap").get(0).play();
+
+            var master = new TimelineMax({
+                delay: 1,
+                repeat: 0,
+            });
+
+            var goToNext = function () {
+                if (next === "dare") {
+                    party.dareChosen();
+                } else {
+                    party.shotChosen();
+                }
+            };
+
+            master.add("start")
+            .to(pole, 0.4, { transform: "translateY(600px)", ease: Back.easeIn }, "start")
+            .to(choice, 0.4, { transform: "translateY(600px)", ease: Back.easeIn }, "start")
+            .add("removeOptions")
+            .to(shot, 0.3, { transform: "scale(0)", ease: Back.easeIn }, "removeOptions")
+            .to(dare, 0.3, { transform: "scale(0)", ease: Back.easeIn }, "removeOptions+=0.2")
+            .addCallback(goToNext);
+        },
+
+        shotChosen: function () {
+            TweenMax.to(".the-shot", 1, { transform: "scale(1)", ease: Bounce.easeOut });
+            TweenMax.to(".flames", 0.5, { transform: "translateY(0)" });
+            party.showReset();
+        },
+
+
+        dareChosen: function () {
+            TweenMax.to(".the-dare", 1, { transform: "scale(1)", ease: Bounce.easeOut });
+            party.showReset();
+        },
+
+        showReset: function () {
+            var show = new TimelineMax({
+                delay: 1,
+                repeat: 0,
+            });
+
+            show.add("start")
+            .to(".reset", 0.5, { transform: "translateY(0)" }, "start+=5");
+        },
     };
 
     $(window).load(function () { party.init(); });
